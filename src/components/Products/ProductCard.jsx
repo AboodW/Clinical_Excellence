f50e3041cb4d3e41791229650210858a5a442a50
Abdullah_Wawi <abed.2alwawi2000@gmail.com>
2023-05-05 23:40:43 +0300

    add context

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -1,8 +1,28 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import "./ProductCard.css";
+import { Link } from "react-router-dom";
+import { urlFor } from "../../lib/client";
 
-const ProductCard = (props) => {
-  return <div className="col-md-3">aa</div>;
+const ProductCard = ({ product: { image, name, slug, price, details } }) => {
+  return (
+    <div className="col-md-3 productColumn">
+      <Link to="/">
+        <div className="card productCard border-0">
+          <div className="productCardTitle">
+            <p className="productBigTitle">{name}</p>
+            <p className="productSmallTitle">{details}</p>
+          </div>
+
+          <img src={urlFor(image && image[0])} alt={name} />
+
+          <div className="productCardPrice">
+            <p className="productPrice">${price}</p>
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default ProductCard;
